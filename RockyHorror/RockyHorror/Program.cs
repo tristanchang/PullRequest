3525ac61d2bd113abca57605ace726ed1e1e8f84
tristanchang <tristanchang@gmail.com>
2022-08-30 14:20:35 +0800

    Instantiate a showing

--- RockyHorror/RockyHorror/Program.cs
+++ RockyHorror/RockyHorror/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿namespace RockyHorror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var showing = new Showing();
+            showing.Location = "Brattle ";
+            showing.NumberOfSeats = 250;
+            showing.ShowTimes = new List<DateTime>
+            {
+                new DateTime (0,0,0,10,0,0),
+                new DateTime (0,0,0,13,0,0),
+                new DateTime (0,0,0,16,0,0),
+                new DateTime (0,0,0,19,0,0),
+                new DateTime (0,0,0,22,0,0),
+                new DateTime (0,0,0,0,0,1)
+            };
+        }
+    }
+}
